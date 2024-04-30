3e17855e6f2229978d8f4bb7e235512a06bd4ce3
carlos <carlosf.corralesz@uqvirtual.edu.co>
2024-04-30 12:36:59 -0500

    se creo la interfaz del nucleo 2

--- conexidad.py
+++ conexidad.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 
+from matplotlib.pyplot import text
+
 from euler import dibujar_grafo
 
 
@@ -10,4 +12,17 @@ def crear_ventana():
     buttoneuler = tk.Button(ventana, text="grafo euleriano", command=dibujar_grafo)
     buttoneuler.pack()
     buttoneuler.place(x=200, y=200)
+    # aui debe de ir el boton para el grafo hamiltoniano
+    button_hamilton = tk.Button(ventana, text="grafo hamiltoniano")
+    button_hamilton.pack()
+    button_hamilton.place(x=200, y=250)
+    # va la función de konisberg
+    button_koonisber = tk.Button(ventana, text="puentes de konisberg")
+    button_koonisber.pack()
+    button_koonisber.place(x=200, y=300)
+    # va la función del terema de dirac
+    button_dirac = tk.Button(ventana, text="teorema de dirac")
+    button_dirac.pack()
+    button_dirac.place(x=200, y=350)
+
     return ventana
--- index.py
+++ index.py
@@ -3,19 +3,25 @@ import tkinter as tk
 from conexidad import crear_ventana
 
 
-def conexidad():
+def conexidad(ventana):
     crear_ventana()
     ventana.destroy()
 
 
-ventana = tk.Tk()
-ventana.title("Introducción a grafos")
-ventana.geometry("600x600")
-cabeza = tk.Label(ventana, text="Escoja la opción que más le guste").pack()
+def ventana_crear():
+    ventana = tk.Tk()
+    ventana.title("Introducción a grafos")
+    ventana.geometry("600x600")
+    cabeza = tk.Label(ventana, text="Escoja la opción que más le guste").pack()
 
-# En el comando del botón, pasa la referencia de la función 'numero' en lugar de llamarla directamente
-button = tk.Button(ventana, text="Dibujar grafo", command=conexidad, fg="red")
-button.pack()
-button.place(x=200, y=200)
+    # En el comando del botón, pasa la referencia de la función 'numero' en lugar de llamarla directamente
+    button = tk.Button(
+        ventana, text="Dibujar grafo", command=lambda: conexidad(ventana), fg="red"
+    )
+    button.pack()
+    button.place(x=200, y=200)
 
-ventana.mainloop()
+    ventana.mainloop()
+
+
+ventana_crear()
